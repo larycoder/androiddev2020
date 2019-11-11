d8713d1c6fcc918c671b0c6cc815595053f8c8af
larycoder <lenhuchuhiep99@gmail.com>
2019-11-11 15:33:30 +0700

    add hello world to app

---
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -0,0 +1,32 @@
+package vn.edu.usth.weather;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ForecastFragment extends Fragment {
+
+
+    public ForecastFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = new View(getContext());
+        view.setBackgroundColor(0xFFFFFF00);
+        return view;
+    }
+
+}
--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class WeatherActivity extends Activity {
+public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         Log.i("Create", "call Create");
+
+        // add hello world
+        TextView msg = (TextView) findViewById(R.id.textView);
+        msg.setText("hello world");
+
+        // create fragment
+        ForecastFragment frag = new ForecastFragment();
     }
 
     @Override
