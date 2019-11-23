6f5947aedff32725b1556fbc9c6c725b377f5d1f
larycoder <lenhuchuhiep99@gmail.com>
2019-11-24 02:53:13 +0700

    lab7: add random property for weather state

--- app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class WeatherFragment extends Fragment {
     public WeatherFragment() {
         // Required empty public constructor
@@ -18,9 +20,12 @@ public class WeatherFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // set string of weather image
+        int[] img = {R.drawable.cloudy, R.drawable.rain, R.drawable.rainy, R.drawable.heavy_rain, R.drawable.sunny};
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_weather, container, false);
-        ((ImageView) view.findViewById(R.id.WeatherFragment)).setImageResource(R.drawable.sunny);
+        ((ImageView) view.findViewById(R.id.WeatherFragment)).setImageResource(img[new Random().nextInt(5)]);
         ((TextView) view.findViewById(R.id.Main_status_weather)).setText("120C\nSunny");
         ((TextView) view.findViewById(R.id.Main_city)).setText("Paris");
         return view;
