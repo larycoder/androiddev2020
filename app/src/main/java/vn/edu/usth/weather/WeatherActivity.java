0ee2b700a5c38cab5d74836818c1a34da87613a0
larycoder <lenhuchuhiep99@gmail.com>
2019-12-03 23:09:43 +0700

    lab 11: store mp3 file to /sdcard/Download with addFileToSdcard function

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,12 +3,25 @@ package vn.edu.usth.weather;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.icu.util.Output;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class WeatherActivity extends AppCompatActivity {
+    // link to storage of file
+    private String PathFile = "/sdcard/Download/nhacdubaothoitietvtc14.mp3";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +39,12 @@ public class WeatherActivity extends AppCompatActivity {
         // create tab layout
         TabLayout tab = findViewById(R.id.tab);
         tab.setupWithViewPager(pager);
+
+        // add file to sdcard
+        addFileToSdcard();
+
+        // play mp3
+//        MediaPlayer mp = MediaPlayer.create(WeatherActivity.this, )
     }
 
     @Override
@@ -56,5 +75,34 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.i("Pause", "call Pause");
+    }
+
+    protected void addFileToSdcard(){
+        // add music to android storage
+        try {
+            // get mp3 file
+            InputStream is = getResources().openRawResource(R.raw.nhacdubaothoitietvtc14);
+            // create file for music in android
+            File file = new File(PathFile);
+            if(!file.exists())
+                file.createNewFile();
+            // push data to buffer
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer);
+
+            // push data to android storage
+            OutputStream os = new FileOutputStream(file);
+            os.write(buffer);
+            os.flush();
+
+            // close file
+            os.close();
+            is.close();
+
+        } catch(FileNotFoundException ex){
+            Log.e("WeatherActivity", "File not found " + ex.toString());
+        } catch(IOException ex){
+            Log.e("WeatherActivity", ex.toString());
+        }
     }
 }
